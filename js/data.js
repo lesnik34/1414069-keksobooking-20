51cde24faa34da08eef979e8f9ea20700cde5338
lesnik34 <lesnyak_2011@mail.ru>
2020-07-16 13:39:57 +0300

    test

--- js/data.js
+++ js/data.js
@@ -3,12 +3,6 @@
 (function () {
 
   window.data = {
-    loadHandler: function (loadData) {
-
-      window.generation.renderPins(loadData);
-      window.generation.renderCard(loadData[0]);
-
-    },
     errorHandler: function (errorMessage) {
       var node = document.createElement('div');
       node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
@@ -18,6 +12,10 @@
       node.style.fontSize = '30px';
       node.textContent = errorMessage;
       document.body.insertAdjacentElement('afterbegin', node);
+    },
+    loadHandler: function (loadData) {
+      window.generation.renderPins(loadData);
+      window.generation.renderCard(loadData[0]);
     }
   };
 

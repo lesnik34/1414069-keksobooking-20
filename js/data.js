c4c0deaa6231cb12a27509fd0525dff9a5b9742f
lesnik34 <lesnyak_2011@mail.ru>
2020-07-16 13:37:30 +0300

    test

--- js/data.js
+++ js/data.js
@@ -4,10 +4,11 @@
 
   window.data = {
     loadHandler: function (loadData) {
-      window.generation.renderPins(loadData);
       window.generation.renderCard(loadData[0]);
+      window.generation.renderPins(loadData);
     },
     errorHandler: function (errorMessage) {
+      
       var node = document.createElement('div');
       node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
       node.style.position = 'absolute';

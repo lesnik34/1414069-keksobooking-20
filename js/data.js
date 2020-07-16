dff2b9b70fe6d2a574fc35d75c79896b553337ef
lesnik34 <lesnyak_2011@mail.ru>
2020-07-16 13:56:27 +0300

    Update data.js

--- js/data.js
+++ js/data.js
@@ -3,6 +3,10 @@
 (function () {
 
   window.data = {
+    loadHandler: function (loadData) {
+      window.generation.renderPins(loadData);
+      window.generation.renderCard(loadData[0]);
+    },
     errorHandler: function (errorMessage) {
       var node = document.createElement('div');
       node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
@@ -12,10 +16,6 @@
       node.style.fontSize = '30px';
       node.textContent = errorMessage;
       document.body.insertAdjacentElement('afterbegin', node);
-    },
-    loadHandler: function (loadData) {
-      window.generation.renderPins(loadData);
-      window.generation.renderCard(loadData[0]);
     }
   };
 
